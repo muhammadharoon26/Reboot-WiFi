df1cb500ee0cf300a6a16ce1ebc160ec4b4c4e60
Muhammad Haroon <mharoon1326@gmail.com>
2025-06-08 15:48:26 +0500

    Remove unused import statement for improved code cleanliness

--- reboot.py
+++ reboot.py
@@ -1,6 +1,5 @@
 from selenium import webdriver
 from selenium.webdriver.common.by import By
-from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.common.alert import Alert
 
 options = webdriver.ChromeOptions()
